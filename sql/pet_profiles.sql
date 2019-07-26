f32894d3af7e745b63eb9aaa7d649eaffff9c07f
Sarah Gu <guwen888@gmail.com>
2019-07-26 13:05:59 +0000

    restart

--- sql/pet_profiles.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS human_pets;
-
-CREATE TABLE human_pets(human_name VARCHAR(100), p_name VARCHAR(100), p_type VARCHAR(100), p_location INT(100), p_hobby VARCHAR(100), p_aniPref INT(100), bio VARCHAR(100), PRIMARY KEY(human_name));
